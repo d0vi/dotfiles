197438a95f18429b7888a770f3b7e60262666fb3
Jesús Iglesias Iglesias <dovimeister@gmail.com>
2024-05-14 22:54:26 +0200

    🪚️ Attemp to install Homebrew interactively

--- run_once_3-install-homebrew.sh
+++ run_once_3-install-homebrew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! command -v brew &> /dev/null; then
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "✅️ Homebrew has been successfully installed"
 else
     echo "✅️ Homebrew is already installed"
