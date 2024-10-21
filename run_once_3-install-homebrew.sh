fd1d09cb3c75897a076fa392eea2b899a4a4416f
Jesús Iglesias Iglesias <dovimeister@gmail.com>
2024-10-21 19:10:01 +0200

    ❌ Do not install Homebrew package manager

--- run_once_3-install-homebrew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ! command -v brew &> /dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "✅️ Homebrew has been successfully installed"
-else
-    echo "✅️ Homebrew is already installed"
-fi
